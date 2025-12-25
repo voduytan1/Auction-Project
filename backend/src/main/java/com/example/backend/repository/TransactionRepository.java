8a58c010b8f8808c8dda6fd01e1632356687a207
voduytan <voduytanbt1@gmail.com>
2025-12-25 20:32:04 +0700

    feat(transaction): add list own buyer and seller transaction

--- backend/src/main/java/com/example/backend/repository/TransactionRepository.java
+++ backend/src/main/java/com/example/backend/repository/TransactionRepository.java
@@ -2,9 +2,16 @@ package com.example.backend.repository;
 
 import com.example.backend.entity.Transaction;
 import org.jetbrains.annotations.NotNull;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.UUID;
+
 @Repository
 public interface TransactionRepository extends JpaRepository<@NotNull Transaction, @NotNull Long> {
+    Page<@NotNull Transaction> findAllByBuyer_Userid(UUID buyerId, Pageable pageable);
+    Page<@NotNull Transaction> findAllBySeller_Userid(UUID sellerId, Pageable pageable);
+
 }
--- backend/src/main/java/com/example/backend/service/TransactionService.java
+++ backend/src/main/java/com/example/backend/service/TransactionService.java
@@ -1,14 +1,22 @@
 package com.example.backend.service;
 
+import com.example.backend.dto.common.PaginationRequest;
 import com.example.backend.dto.transaction.TransactionResponse;
 import com.example.backend.entity.Transaction;
 import com.example.backend.exception.ForbiddenException;
 import com.example.backend.mapper.TransactionMapper;
 import com.example.backend.repository.TransactionRepository;
 import jakarta.persistence.EntityNotFoundException;
+import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
+import org.jetbrains.annotations.NotNull;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.ModelAttribute;
 
+import java.util.List;
 import java.util.UUID;
 
 @Service
@@ -30,11 +38,28 @@ public class TransactionService {
         return transactionRepository.save(transaction);
     }
 
-    public TransactionResponse findOne(Long id, String userid){
+    public TransactionResponse findOne(Long id, UUID userid){
         Transaction transaction = transactionRepository.findById(id).orElseThrow(()->new EntityNotFoundException("Không tìm thấy giao dịch với id "+ id));
-        if(userid.equals(transaction.getBuyer().getUserid())||userid.equals(transaction.getSeller().getUserid())){
+        if(!userid.equals(transaction.getBuyer().getUserid())&&!userid.equals(transaction.getSeller().getUserid())){
             throw new ForbiddenException("Không có quyền truy cập giao dịch này");
         }
         return transactionMapper.toResponse(transaction);
+    }
+
+    public Page<@NotNull TransactionResponse> findOwnBuyerTransactions(Pageable pageable, UUID userid) {
+        Page<@NotNull Transaction> transactions = transactionRepository.findAllByBuyer_Userid(userid, pageable);
+        List<TransactionResponse> list = transactions.getContent().stream()
+                .map(transactionMapper::toResponse)
+                .toList();
+        return new PageImpl<>(list, pageable, transactions.getTotalElements());
+    }
+
+    public Page<@NotNull TransactionResponse> findOwnSellerTransactions(Pageable pageable, UUID userid) {
+        Page<@NotNull Transaction> transactions = transactionRepository.findAllBySeller_Userid(userid, pageable);
+        List<TransactionResponse> list = transactions.getContent().stream()
+                .map(transactionMapper::toResponse)
+                .toList();
+        return new PageImpl<>(list, pageable, transactions.getTotalElements());
+
     }
 }
